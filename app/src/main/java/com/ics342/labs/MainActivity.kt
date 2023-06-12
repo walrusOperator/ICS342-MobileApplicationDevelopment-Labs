f4860633527c06af752900b2827774dbce3fb3da
walrusOperator <indierevolver@gmail.com>
2023-06-12 13:17:50 -0500

    Lab 3 commit

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -3,13 +3,23 @@ package com.ics342.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ics342.labs.data.DataItem
 import com.ics342.labs.ui.theme.LabsTheme
 
@@ -43,7 +53,8 @@ class MainActivity : ComponentActivity() {
             LabsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    DataItemList(dataItems = dataItems)
                 }
             }
         }
@@ -61,11 +72,27 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun DataItemView(dataItem: DataItem) {
     /* Create the view for the data item her. */
+    Row (
+        Modifier.padding(bottom = 8.dp)){
+        Text(text = dataItem.id.toString())
+        Spacer(modifier = Modifier.size(8.dp))
+
+        Column {
+            Text(text = dataItem.name, style = TextStyle(fontWeight = Bold))
+            Text(text = dataItem.description, style = TextStyle(fontSize = 12.sp))
+        }
+    }
+
 }
 
 @Composable
 fun DataItemList(dataItems: List<DataItem>) {
     /* Create the list here. This function will call DataItemView() */
+    LazyColumn {
+        for(data in dataItems){
+            item{DataItemView(data)}
+        }
+    }
 }
 
 @Preview(showBackground = true)
