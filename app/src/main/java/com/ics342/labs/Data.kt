9e6d54582ea7a7e0113d8449355ddf5d7e40d1e2
Thomas Mullins <85575321+walrusOperator@users.noreply.github.com>
2023-07-10 19:03:38 -0500

    updated data class, began lazyColumn

--- app/src/main/java/com/ics342/labs/Data.kt
+++ app/src/main/java/com/ics342/labs/Data.kt
@@ -1,9 +1,10 @@
 package com.ics342.labs
 
 // Add the data classes here.
-data class personalDetails (
-    val id : Int,
-    val firstName : String,
-    val lastName : String,
-    val age : Int
-        )
+data class PersonalDetails (
+    @Json(name = "id") val id : Int,
+    @Json(name = "given_name") val firstName : String,
+    @Json(name = "family_name") val lastName : String,
+    @Json(name = "age") val age : Int
+)
+
--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import com.ics342.labs.ui.theme.LabsTheme
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +30,20 @@ class MainActivity : ComponentActivity() {
                     /*
                     Display the items from the Json file in a LazyColumn
                      */
+
+                    lazyColumn {
+                        for(person in data){
+
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+private fun personView() {
+
 }
 
 private fun loadData(resources: Resources): String {
@@ -40,10 +53,19 @@ private fun loadData(resources: Resources): String {
         .use { it.readText() }
 }
 
-private fun dataFromJsonString(json: String): List<personalDetails> {
+private fun dataFromJsonString(json: String): List<PersonalDetails> {
     val moshi: Moshi = Moshi.Builder().build()
-    val jsonAdapter: JsonAdapter<List<personalDetails>> = moshi.adapter<List<personalDetails>>()
+    val jsonAdapter: JsonAdapter<List<PersonalDetails>> = moshi.adapter<List<PersonalDetails>>()
     return jsonAdapter.fromJson(json)
 }
 
+//@Composable
+//fun DataItemList(dataItems: List<DataItem>, navController : NavController) {
+//    /* Create the list here. This function will call DataItemView() */
+//    LazyColumn {
+//        for(data in dataItems){
+//            item{DataItemView(data, navController = navController)}
+//        }
+//    }
+//}
 
