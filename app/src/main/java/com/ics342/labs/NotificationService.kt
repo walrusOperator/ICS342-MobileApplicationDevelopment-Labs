910285be6937ad2e6a061ce19b83638bd9d7a857
Thomas Mullins <85575321+walrusOperator@users.noreply.github.com>
2023-07-31 14:33:56 -0500

    Lab submission

--- app/src/main/java/com/ics342/labs/NotificationService.kt
+++ app/src/main/java/com/ics342/labs/NotificationService.kt
@@ -3,9 +3,12 @@ package com.ics342.labs
 import android.Manifest.permission
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -14,8 +17,8 @@ import java.util.UUID
 
 class NotificationService: Service() {
 
-    private val notificationManager: NotificationManagerCompat =
-        NotificationManagerCompat.from(this)
+//    private val notificationManager: NotificationManagerCompat =
+//        NotificationManagerCompat.from(this)
 
     override fun onCreate() {
         super.onCreate()
@@ -36,7 +39,19 @@ class NotificationService: Service() {
 
        // Build notification
 
-        TODO("Build and show notification")
+//        TODO("Build and show notification")
+        val pendingIntent : PendingIntent = PendingIntent.getActivity(
+            this,0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val noteBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.star)
+            .setContentTitle(CHANNEL_ID)
+            .setContentText(NOTIFICATION_ID.toString())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(startId, noteBuilder.build())
+        }
         return START_STICKY_COMPATIBILITY
     }
 
@@ -46,7 +61,16 @@ class NotificationService: Service() {
     }
 
     private fun createNotificationChannel() {
-        TODO("Create notification channel and register with the system")
+//        TODO("Create notification channel and register with the system")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notification = NOTIFICATION_ID.toString()
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, notification, importance).apply {
+                description  = notification
+            }
+            val notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     companion object {
